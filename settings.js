830f0953c670784bc3919e9074a83339f6c62459
TAKUNDAELIJAH <168351848+TAKUNDAELIJAH@users.noreply.github.com>
2025-08-14 13:52:12 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: '',
-  author: '‎',
-  botName: ".",
-  botOwner: '103', // Your name
-  ownerNumber: '263780145644', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'Taku',
+  author: 'Taku‎',
+  botName: "Taku",
+  botOwner: '263781265449', // Your name
+  ownerNumber: '263781265449', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
